ba59158077a55c727a33b782593ada1c7d528d15
luvpreetsingh <s.luvpreet33@gmail.com>
2020-10-05 13:42:51 +0530

    add debug statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 IMAGE_NAME="$INPUT_GCR_HOST/$GOOGLE_PROJECT_ID/$INPUT_IMAGE_NAME:$GITHUB_SHA"
 echo "here we go"
+echo $GITHUB_SHA
+echo $IMAGE_NAME
 
 echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
 docker build -t $IMAGE_NAME .
